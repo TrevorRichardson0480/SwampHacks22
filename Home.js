59b6d412793553136b23b1db8428d5da08e237ec
jordan gillispie <jordangillispie@jordans-MacBook-Air.local>
2022-01-29 16:18:13 -0500

    adding code to new branch

--- Home.js
+++ Home.js
@@ -3,14 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
-import { micStart } from './audio.js'
+import { startAudioRecording } from './audio.js'
 
 const Home = () => {
     return (
         <div>
-          <button onClick={micStart} type="button" class="btn btn-secondary btn-circle btn-xl">
-            <span class="mic">
-              <FontAwesomeIcon class="mic" icon={faMicrophone} />
+          <button onClick={startAudioRecording} type="button" className="btn btn-secondary btn-circle btn-xl">
+            <span className="mic">
+              <FontAwesomeIcon className="mic" icon={faMicrophone} />
             </span>
           </button>
         </div>
--- audio.js
+++ audio.js
@@ -1,22 +1,41 @@
-const audio = document.getElementById('audio')
-const stream = navigator.mediaDevices.getUserMedia({audio : true})
+ /** Starts the audio recording*/
+ export function startAudioRecording() {
+     console.log('okay now we are starting')
 
-function microphoneAccess() {
-    // give some contraints: {audio:true}
-    navigator.mediaDevices.getUserMedia ({
-        audio : true,
-        video: false
-        // when we accept, get a stream back
-        }).then(stream => {
-            const mediaRecorder = new MediaRecorder(stream)
-            mediaRecorder.start()
-            // find the audio element
-            audio.srcObject = stream;
-        }).catch(console.error)
-}
+     // get access first and create a stream 
+     navigator.mediaDevices.getUserMedia({
+             audio: true
+         })
+         .then(stream => {
+             const mediaRecorder = new MediaRecorder(stream);
+             mediaRecorder.start();
 
-function micStart() {
- audio.start();
-}
+             // we will need to store the audio continuously 
+             const userQuestion = [];
+             // when data is available, lets add it to chunks
+             mediaRecorder.addEventListener("dataavailable", event => {
+                 userQuestion.push(event.data);
+             });
+             // after we stop lets store the chunks in a blob
+             // create a url out of the blob 
+             // play it back to the user 
+             mediaRecorder.addEventListener("stop", () => {
+                 const blob = new Blob(userQuestion);
+                 const audioUrl = URL.createObjectURL(blob);
+                 const audio = new Audio(audioUrl);
+                 audio.play();
 
-window.addEventListener('load', microphoneAccess, false)
+             });
+             // use setTimeout to stop the recording after specified time 
+             setTimeout(() => {
+                 console.log("3 seconds have passed")
+                 mediaRecorder.stop();
+             }, 12000);
+
+         });
+ }
+
+ // next task is to figure out when to start and stop recording
+ // be able to store the recording as it happens and finish storing whenever we .stop()
+ // .requestData
+ // we also need to not start recording as soon as we enter the site 
